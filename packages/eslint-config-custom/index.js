768298a82965cd7689d9d415974e9cb9e78dbec1
rvndl <rvndldev@gmail.com>
2022-11-17 18:02:47 +0100

    initial commit

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next", "turbo"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
