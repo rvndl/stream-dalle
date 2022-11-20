9fcb666eae26b9b1353b0b3f56e90d7251f747aa
rvndl <rvndldev@gmail.com>
2022-11-20 14:05:06 +0100

    feat(logs): loading indicator

--- apps/web/src/components/dashboard/logs.tsx
+++ apps/web/src/components/dashboard/logs.tsx
@@ -1,12 +1,18 @@
 import { trpc } from "../../utils/tprc";
-import { Badge, Card } from "../ui";
+import { Badge, Card, Spinner } from "../ui";
 
 export const Logs = () => {
-  const { data } = trpc.auth.logs.useQuery();
+  const { data, isLoading } = trpc.auth.logs.useQuery();
 
   return (
     <Card title="Logs" className="flex flex-1 self-start">
       <div className="mt-4 flex flex-col w-full gap-2">
+        {isLoading && (
+          <p className="text-gray-400 font-semibold flex items-center">
+            <Spinner className="h-6 w-6 text-purple-400 mr-2" />
+            Loading...
+          </p>
+        )}
         {data?.map((log) => (
           <div className="p-2.5 rounded-md bg-gray-600 flex justify-between flex-row w-full">
             <p className="font-semibold text-sm">{log.redeemer}</p>
